caaa54dff7f2478184c6c2ba3a6b086123981ea0
zeyfoxxx <lucasbenady@gmail.com>
2025-02-27 11:17:39 +0100

    faut que sa marche

--- index.php
+++ index.php
@@ -10,78 +10,86 @@
         <link rel="stylesheet" href="https://cdn.anychart.com/releases/v8/css/anychart-ui.min.css" />
         <link rel="stylesheet" href="https://cdn.anychart.com/releases/v8/fonts/css/anychart.min.css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="projet.css">
         <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="visuel.css">
 
-    </head>
-    <body>
-  
+    </head>    
+    <body>    
+        <header class="navbar navbar-expand-lg navbar-light bg-secondary">
+        <a href="#" class="navbar-brand">
+            <img src="image/capi.jpg" alt="capi" class="w-25" />
+        </a>
         
-    <header class="navbar navbar-expand-md navbar-light mr-auto">
-    <a href="#" class="navbar-brand ml-25">
-         <img src="image/sofiane.jpg" alt="sofiane" class="w-25" />
-     </a>
-	 <!-- id nomme les elts a faire disparaitre -->
-     <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-content">
-         <span class="navbar-toggler-icon"></span>
-     </button>
-	 <!-- ici id nomme les elements geres par button -->
-     <div class="collapse navbar-collapse" id="navbar-content">
-     <!--Begin main nav-->
-      <nav>
-         <ul class="navbar-nav">
-           <li class="nav-item">
-               <a href="#" class="nav-link active">Accueil</a>
-           </li>
-           <li class="nav-item">
-               <a href="#" class="nav-link">donnée</a>
-           </li>
-           <li class="nav-item">
-               <a href="#" class="nav-link">ADMIN</a>
-           </li>
-           <li class="nav-item">
-               <a href="#" class="nav-link">test</a>
-           </li>
-       </ul>
-      </nav>
-      <!--End main nav-->
-      <!--Begin search form-->
-      <!--End search form-->
-     </div><!--End .navbar-collapse-->
-     
-  </header>
+        <!-- Bouton hamburger pour mobile -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-        <div id="container"></div>
-            <script>
-                anychart.data.loadJsonFile("data.php", function (data) {  // init and draw chart
-                    var chart = anychart.line(data);
-                    chart.title("Top 5 fruits");
-                    chart.container("container");
-                    chart.draw();
-
-                    // update chart from server every 5 seconds
-                    setInterval(function(){
-                        // make request to server
-                        // to use loadJsonFile function you must include data-adapter.min.js to your page
-                        anychart.data.loadJsonFile("data.php", function (data) {
-                            chart.data(data);
-                        })
-                    }, 5000);
-                });
-            </script>
-        
-        <div id="containerM">
-            <header>
-                <h1> TEST</h1>
-            </header>
-    
-    
+        <!-- Contenu de la navbar -->
+        <div class="collapse navbar-collapse" id="navbar-content">
+            <ul class="navbar-nav ml-auto mr-5"> <!-- Ajout de ml-auto ici -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link active">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Donnée</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">ADMIN</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Test</a>
+                </li>
+            </ul>
         </div>
+        
+    </header>
+
+    <div class="container-fluid">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-8 col-sm-12">
+                <div id="container" style="width: 100%; height: 50vh;"></div>
+            </div>
+        </div>
+    </div>
+                
+                <script>
+                    anychart.data.loadJsonFile("data.php", function (data) {  // init and draw chart
+                        var chart = anychart.line(data);
+                        chart.title("Top 5 fruits");
+                        chart.background().fill("rgb(173, 188, 230)");
+                        chart.container("container");
+                        chart.draw();
+
+                        // update chart from server every 5 seconds
+                        setInterval(function(){
+                            // make request to server
+                            // to use loadJsonFile function you must include data-adapter.min.js to your page
+                            anychart.data.loadJsonFile("data.php", function (data) {
+                                chart.data(data);
+                            })
+                        }, 5000);
+                    });
+                </script>
+            
+            <div id="containerM">
+
+            
+            </div>
+        
+        
+        
+        <script src="js/fontawesome-all.min.js"></script>
+        <script src="js/jquery-3.3.1.slim.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        
+        
+        <footer class="bg-secondary text-white text-center py-3 fixed-bottom">
+            <p>&copy; caca balise capitainerie bandole BTS CIEL REMPART</p>
+        </footer>
     
     
-    
-    <script src="js/fontawesome-all.min.js"></script>
-    <script src="js/jquery-3.3.1.slim.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
     </body>
+
+
 </html>
