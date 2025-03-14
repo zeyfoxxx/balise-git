7ea898e3af1ee041f608763ebc199c90cf8c1064
zeyfoxxx <lucasbenady@gmail.com>
2025-03-14 16:15:43 +0100

    rajout de la page credit

---
+++ credit.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>balise bandol</title>
+        <meta charset="utf-8">
+        <script src="https://cdn.anychart.com/releases/v8/js/anychart-base.min.js"></script>
+        <script src="https://cdn.anychart.com/releases/v8/js/anychart-data-adapter.min.js"></script>
+        <script src="https://cdn.anychart.com/releases/v8/js/anychart-exports.min.js"></script>
+        <script src="https://cdn.anychart.com/releases/v8/js/anychart-vml.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.anychart.com/releases/v8/css/anychart-ui.min.css" />
+        <link rel="stylesheet" href="https://cdn.anychart.com/releases/v8/fonts/css/anychart.min.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="visuel.css">
+
+    </head>    
+    <body>    
+        <header class="navbar navbar-expand-lg navbar-light bg-secondary">
+        <a href="#" class="navbar-brand">
+            <img src="image/capi.jpg" alt="capi" class="w-25" />
+        </a>
+        
+        <!-- Bouton hamburger pour mobile -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Contenu de la navbar -->
+        <div class="collapse navbar-collapse" id="navbar-content">
+            <ul class="navbar-nav ml-auto mr-5"> <!-- Ajout de ml-auto ici -->
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link active">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a href="donnee.php" class="nav-link">Donnée</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">ADMIN</a>
+                </li>
+                <li class="nav-item">
+                    <a href="credit.php" class="nav-link">CREDIT</a>
+                </li>
+            </ul>
+        </div>
+        
+    </header>
+            
+            <div id="containerB" class="d-flex flex-column justify-content-center align-items-center text-white text-center bg-dark font-weight-bolder" style="height: 65vh">
+                <p class="h2"> Ce site a été créé par les élèves du BTS CIEL deuxième année au lycée Rempart dans le 7ème arrondissement de Marseille. Il a pour but de valider leur projet de fin d'année afin de valider leur obtention du BTS </p>
+                <p class="h2"> Il a pour but de valider leur projet de fin d'année afin de valider leur obtention du BTS, ou ils avait pour but de proposer une solution au plaisancier pour qu'il puisse vérifier l'état actuel de la mer.</p>
+                <p class="h2"> On espère donc que ce site vous sera utile et que vous l'apprécierez.</p>
+                <p class="h2"> En vous souhaitant une bonne journée et bonne navigation !</p>
+                
+            
+            </div>
+        
+        
+        
+        <script src="js/fontawesome-all.min.js"></script>
+        <script src="js/jquery-3.3.1.slim.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        
+        
+        <footer class="bg-secondary text-white text-center py-3">
+            <p>&copy;balise capitainerie bandole BTS CIEL REMPART</p>
+            <p> Ce site a était réaliser par les étudiants du BTS CIEL Rempart</p>
+        </footer>
+    
+    
+    </body>
+
+
+</html>
